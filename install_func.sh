460a6b2aca4ee276f53c15092b1e9e449ea4e204
Jithran <info@jithransikken.nl>
2022-07-09 00:07:20 +0200

    fix: typo in package name

--- install_func.sh
+++ install_func.sh
@@ -126,7 +126,7 @@ install_docker() {
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 	sudo apt update
-	sudo apt -y install docker-ce docker-composer
+	sudo apt -y install docker-ce docker-compose
 	sudo groupadd docker
 	sudo usermod -aG docker $USER
 }
