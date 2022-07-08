e46e4aa90fbf047942ce511017a9bbbda6b622ec
Jithran <info@jithransikken.nl>
2022-07-08 22:43:39 +0200

    fix: gh installation

--- install_func.sh
+++ install_func.sh
@@ -62,11 +62,11 @@ install_neovim() {
 }
 
 install_gh() {
-	curl -fsSL https://cli.github.compackages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 	sudo apt update
-	sudo apt -y install gh/
+	sudo apt -y install gh
 }
 
 install_php() {
