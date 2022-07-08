9427108fc88fde2df0531ad2898828fb7691c76a
Jithran <info@jithransikken.nl>
2022-07-08 18:26:49 +0000

    github cli tool installation

--- install_func.sh
+++ install_func.sh
@@ -59,6 +59,14 @@ install_neovim() {
 	nvim +PlugInstall +qall
 }
 
+install_gh() {
+	curl -fsSL https://cli.github.compackages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+	sudo apt update
+	sudo apt -y install gh/
+}
+
 install_generic() {
 	sudo apt update && sudo apt -y upgrade
 	# install dependencies
