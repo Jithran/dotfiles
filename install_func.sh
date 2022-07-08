8da184772aacbebae35e9b2f9cc91ed91f6314dc
Jithran <info@jithransikken.nl>
2022-07-08 22:15:29 +0200

    add: php & composer install

--- install_func.sh
+++ install_func.sh
@@ -17,6 +17,8 @@ menu() {
 	Do you want to install vim or NeoVim?
 	$(ColorGreen '1)') Vim
 	$(ColorGreen '2)') NeoVim
+	$(ColorGreen '3)') PHP 8.1 & Composer
+	$(ColorGreen '4)') Github CLI (gh)
 	$(ColorGreen '0)') Cancel and Exit
 	$(ColorBlue 'Choose your editor: ')"
 
@@ -65,6 +67,18 @@ install_gh() {
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 	sudo apt update
 	sudo apt -y install gh/
+}
+
+install_php() {
+
+	sudo apt -y install --no-install-recommends php8.1
+	sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+
+	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+	php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+	php composer-setup.php
+	php -r "unlink('composer-setup.php');"
+	sudo mv composer.phar /usr/local/bin/composer
 }
 
 install_generic() {
