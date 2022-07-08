64839c3cc71f929a776367a5d45f655a8ad9a1f7
Jithran <info@jithransikken.nl>
2022-07-08 23:06:25 +0000

    mod: add checks for package reinstall

--- install_func.sh
+++ install_func.sh
@@ -11,6 +11,15 @@ ColorGreen(){
 ColorBlue(){
 	echo -ne $blue$1$clear
 }
+ColorRed(){
+	echo -ne $red$1$clear
+}
+
+Alert() {
+		ColorRed "\n\n\t###########################################################\n"
+		ColorRed "\t#### $1 \n"
+		ColorRed "\t###########################################################\n\n"
+}
 
 # Menu function
 menu() {
@@ -25,7 +34,7 @@ menu() {
 	$(ColorGreen '7)') Install Snap & packages
 	$(ColorGreen '9)') Install all (with NeoVim)
 	$(ColorGreen '0)') Cancel and Exit
-	$(ColorBlue 'Choose your editor: ')"
+	$(ColorBlue 'Choose your option: ')"
 
 	read editor
 }
@@ -41,109 +50,148 @@ install_complete() {
 }
 
 install_vim() {
-	echo 'installing vim'
-	install_generic
+	if ! command -v vim &> /dev/null
+	then
+		alias vim="nvim"
+		echo 'installing vim'
+		install_generic
 
-	sudo add-apt-repository ppa:jonathonf/vim
-	sudo apt update && sudo apt -y upgrade
-	sudo apt -y install vim font-powerline
+		sudo add-apt-repository ppa:jonathonf/vim
+		sudo apt update && sudo apt -y upgrade
+		sudo apt -y install vim fonts-powerline
 
-	ln -s $PWD/.vimrc $INSTALLDIR/.vimrc 2> /dev/null
-	ln -s $PWD/.vim $INSTALLDIR/.vim 2> /dev/null
+		ln -s $PWD/.vimrc $INSTALLDIR/.vimrc 2> /dev/null
+		ln -s $PWD/.vim $INSTALLDIR/.vim 2> /dev/null
 
-	if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
-		git submodule add -f https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+		if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+			git submodule add -f https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+		fi
+
+		vim +PluginInstall +qall
+
+		# git settings
+		git config --global core.editor "vim"
+	else
+		clear
+		Alert 'Command vim already installed'
 	fi
-
-	vim +PluginInstall +qall
-
-	# git settings
-	git config --global core.editor "vim"
-
 }
 
 install_neovim() {
-	echo 'installing neovim'
-	install_generic
+	if ! command -v nvim &> /dev/null
+	then
+		echo 'installing neovim'
+		install_generic
 
-	sudo apt -y install neovim exuberant-ctags
-	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+		sudo apt -y install neovim exuberant-ctags
+		sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-	if [[ ! -d "$HOME/.config/" ]]; then
-		mkdir -p "$HOME/.config/"
+		if [[ ! -d "$HOME/.config/" ]]; then
+			mkdir -p "$HOME/.config/"
+		fi
+
+		if [[ ! -d "$HOME/.config/nvim/" ]]; then
+			mkdir -p "$HOME/.config/nvim/"
+		fi
+
+		ln -s $PWD/init.vim $INSTALLDIR/.config/nvim/init.vim 2> /dev/null
+
+		nvim +PlugInstall +qall
+	else
+		clear
+		Alert 'Command nvim already installed'
 	fi
-
-	if [[ ! -d "$HOME/.config/nvim/" ]]; then
-		mkdir -p "$HOME/.config/nvim/"
-	fi
-
-	ln -s $PWD/init.vim $INSTALLDIR/.config/nvim/init.vim 2> /dev/null
-
-	nvim +PlugInstall +qall
 }
 
 install_gh() {
-	curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-	sudo apt update
-	sudo apt -y install gh
+	if ! command -v gh &> /dev/null
+	then
+		curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+		sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+		sudo apt update
+		sudo apt -y install gh
+	else
+		clear
+		Alert 'Command gh already installed'
+	fi
 }
 
 install_php() {
+	if ! command -v php &> /dev/null
+	then
+		sudo apt -y install --no-install-recommends php8.1
+		sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 
-	sudo apt -y install --no-install-recommends php8.1
-	sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
-
-	php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-	php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-	php composer-setup.php
-	php -r "unlink('composer-setup.php');"
-	sudo mv composer.phar /usr/local/bin/composer
+		php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+		php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+		php composer-setup.php
+		php -r "unlink('composer-setup.php');"
+		sudo mv composer.phar /usr/local/bin/composer
+	else
+		clear
+		Alert 'Command PHP already installed'
+	fi
 }
 
 install_zsh() {
-	#installing ZSH
-	sudo apt -y install zsh
-	sudo apt-get -y install powerline fonts-powerline
+	if ! command -v zsh &> /dev/null
+	then
+		#installing ZSH
+		sudo apt -y install zsh
+		sudo apt-get -y install powerline fonts-powerline
 
-	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+		git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-	cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-	sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+		cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+		sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 
-	chsh -s /bin/zsh
+		chsh -s /bin/zsh
 
-	#zsh higlighter 
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
-	cat $PWD/.zshrc_post >> $INSTALLDIR/.zshrc
+		#zsh higlighter 
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+		cat $PWD/.zshrc_post >> $INSTALLDIR/.zshrc
+	else
+		clear
+		Alert 'Command zsh already installed'
+	fi
 }
 
 install_docker() {
-	#install docker support
-	sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-	sudo apt update
-	sudo apt -y install docker-ce docker-compose
-	sudo groupadd docker
-	sudo usermod -aG docker $USER
+	if ! command -v docker &> /dev/null
+	then
+		#install docker support
+		sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+		sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+		sudo apt update
+		sudo apt -y install docker-ce docker-compose
+		sudo groupadd docker
+		sudo usermod -aG docker $USER
+	else
+		echo 'Command docker already installed'
+	fi
 }
 
 install_snap() {
-	#install Snap packages
-	sudo apt -y install snapd
-	sudo snap install snap-store
-	sudo snap install hi
-	sudo snap install bpytop
-	sudo snap connect bpytop:mount-observe
-	sudo snap connect bpytop:network-control
-	sudo snap connect bpytop:hardware-observe
-	sudo snap connect bpytop:system-observe
-	sudo snap connect bpytop:process-control
-	sudo snap connect bpytop:physical-memory-observe
-	sudo snap install emote #install Emote snap package 🤞
+	if ! command -v snap &> /dev/null
+	then
+		#install Snap packages
+		sudo apt -y install snapd
+		sudo snap install snap-store
+		sudo snap install hi
+		sudo snap install bpytop
+		sudo snap connect bpytop:mount-observe
+		sudo snap connect bpytop:network-control
+		sudo snap connect bpytop:hardware-observe
+		sudo snap connect bpytop:system-observe
+		sudo snap connect bpytop:process-control
+		sudo snap connect bpytop:physical-memory-observe
+		sudo snap install emote #install Emote snap package 🤞
+	else
+		echo 'Command snap already installed'
+	fi
 }
 
 install_generic() {
