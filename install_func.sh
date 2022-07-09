cbcf0a4b6974a2ab2b51f0ebf1c5e72726f47397
Jithran <info@jithransikken.nl>
2022-07-10 00:58:37 +0200

    mod: latest version of neovim

--- install_func.sh
+++ install_func.sh
@@ -83,7 +83,13 @@ install_neovim() {
 		echo 'installing neovim'
 		install_generic
 
-		sudo apt -y install neovim exuberant-ctags
+		sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+		git clone https://github.com/neovim/neovim
+		cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+		sudo make install
+		cd $HOME
+		
+		sudo apt -y install exuberant-ctags
 		sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 			https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
