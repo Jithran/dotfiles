4ab6422ea384b885d38495e7e0477f7ef1943119
Jithran <info@jithransikken.nl>
2025-10-14 22:56:10 +0200

    -add: ctrl g for git directories file search in telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -18,6 +18,7 @@ return {
             -- Telescope plugin settings
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<C-p>', builtin.find_files)
+            vim.keymap.set('n', '<C-g>', builtin.git_files)
             vim.keymap.set('n', '<leader>buf', builtin.buffers)
             vim.keymap.set('n', '<leader>gp', builtin.live_grep)
 
