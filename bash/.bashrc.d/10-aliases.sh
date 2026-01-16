f8031779176ab698b8e2adb5e7d742414d5d982e
Jithran <info@jithransikken.nl>
2026-01-16 22:14:42 +0100

    -add: tmux bash aliases

--- bash/.bashrc.d/10-aliases.sh
+++ bash/.bashrc.d/10-aliases.sh
@@ -24,3 +24,36 @@ alias masta=" \
     tmux rename-window \$(basename \$(pwd)); \
     "
 
+# Tmux helpers
+alias tmux-reindex="tmux move-window -r"
+
+# Rename current tmux window to show directory > app (panes)
+tmux-pwd() {
+    local dir=$(tmux display-message -p "#{pane_current_path}")
+    local app=$(tmux display-message -p "#{pane_current_command}")
+    local panes=$(tmux display-message -p "#{window_panes}")
+    local dir_name=$(basename "$dir")
+
+    if [ "$panes" -gt 1 ]; then
+        tmux rename-window "${dir_name} > ${app} (${panes})"
+    else
+        tmux rename-window "${dir_name} > ${app}"
+    fi
+}
+
+# Rename all tmux windows to show directory > app (panes)
+tmux-pwd-all() {
+    tmux list-windows -F "#{window_index}" | while read -r window_index; do
+        local dir=$(tmux list-panes -t "$window_index" -F "#{pane_current_path}" | head -n 1)
+        local app=$(tmux list-panes -t "$window_index" -F "#{pane_current_command}" | head -n 1)
+        local panes=$(tmux list-panes -t "$window_index" | wc -l)
+        local dir_name=$(basename "$dir")
+
+        if [ "$panes" -gt 1 ]; then
+            tmux rename-window -t "$window_index" "${dir_name} > ${app} (${panes})"
+        else
+            tmux rename-window -t "$window_index" "${dir_name} > ${app}"
+        fi
+    done
+}
+
