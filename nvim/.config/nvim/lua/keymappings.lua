205d2f5a35563a8859ded89bf18b98bfd7239dcf
Jithran <info@jithransikken.nl>
2026-01-23 20:10:49 +0100

    -mod: update + keymappings

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -2,7 +2,9 @@
 -- vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
 vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Bottom", remap = true })
-
+vim.keymap.set("n", "<leader><Space>", ":noh<CR>", { desc = "disable the highlighting of current text" })
+vim.keymap.set("n", "<leader>o", ":update<cr> :so<cr>", { desc = "update file and source" })
+vim.keymap.set("n", "<leader>wq", ":write<cr> :qa<cr>", { desc = "Write file and quit all" })
 
 -- reformat file with current pointer memory
 vim.keymap.set('n', '<leader>f', function()
